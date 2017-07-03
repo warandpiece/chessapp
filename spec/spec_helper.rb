33214ed929ec0680952ebddaa124ab41a798c2c2
Brian Wilson <bountonw@gmail.com>
2017-07-03 12:08:01 -0700

    Add database cleaner gem

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -21,6 +21,18 @@ SimpleCov.start
 #
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
 RSpec.configure do |config|
+  # Configure database cleaner
+  config.before(:suite) do
+    DatabaseCleaner.strategy = :transaction
+    DatabaseCleaner.clean_with(:truncation)
+  end
+
+  config.around(:each) do |example|
+    DatabaseCleaner.cleaning do
+      example.run
+    end
+  end
+
   # rspec-expectations config goes here. You can use an alternate
   # assertion/expectation library such as wrong or the stdlib/minitest
   # assertions if you prefer.
