cd96c7e8c7d420f32f96fed6072032fb4fd8ed97
Brian Wilson <bountonw@gmail.com>
2017-07-04 13:58:24 -0700

    Add parentheses around methods in FactoryGirl

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,11 +1,11 @@
 FactoryGirl.define do  
 
   factory :user do
-    sequence :email { |n| "dummyEmail#{n}@gmail.com" }
+    sequence(:email) { |n| "dummyEmail#{n}@gmail.com" }
     password "secretPassword"
     password_confirmation "secretPassword"
     first_name "John"
-    sequence :username { |n| "chessmaster#{n}" }
+    sequence(:username) { |n| "chessmaster#{n}" }
   end
 
   factory :game do
