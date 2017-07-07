b941f5d13830f9dc68d2beed259146d592d070a7
Alyson Stiffel <alysonstiffel@gmail.com>
2017-07-07 12:45:10 -0400

    static_pages controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'pages#index'
+  # resource :dashboard, only: :show
   resources :games, only: [:index, :show, :new, :create, :update]
   resources :moves, :pieces
 end
