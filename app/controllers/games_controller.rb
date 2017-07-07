b90d90988f0bce8f5f606c1ef3dbc1a002c5e176
Brian Wilson <bountonw@gmail.com>
2017-07-07 10:56:37 -0700

    Refactor games_controller_spec (#26)
    This includes a migration to rename `white_player` and `black_player` to `white_player_id` and `black_player_id` respectively.

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -2,7 +2,7 @@ class GamesController < ApplicationController
   before_action :authenticate_user!
 
   def new
-    @game = Game.new  
+    @game = Game.new
   end
 
   def create
@@ -10,20 +10,26 @@ class GamesController < ApplicationController
 
     if @game.save
       render :show, status: :created
+      #redirect_to @game, notice: 'Game was successfully created.'
     else
       render :new, status: :unprocessable_entity # 422
     end
   end
 
   def update
-    @game = current_user.games.find_by(id: params[:id])
-    return render_not_found if @game.blank?
-    @game.update_attributes(game_params)
+    @game = Game.find_by(id: params[:id])
+
+    if @game
+      @game.update(game_params)
+      redirect_to @game, notice: 'Game was successfully updated.'
+    else
+      render :edit
+    end
   end
 
   private
 
   def game_params
-    params.require(:game).permit(:white_player, :black_player, :game_status)
+    params.require(:game).permit(:white_player_id, :black_player_id, :game_status)
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
   it "FactoryGirl to create valid user" do 
-    user = create(:user)
+    user = create(:user, email: "randomemail@email.com")
 
     expect(user).to be_valid
     expect(user.first_name).to eq("John")
