0bf53926d2e2b72dcb07f0ccafdb32073bdddf46
Jacqueline Rosains <jrosains@gmail.com>
2017-07-13 22:22:08 +0000

    added horizontal and vertical

--- spec/factories/pieces.rb
+++ spec/factories/pieces.rb
@@ -3,4 +3,15 @@ FactoryGirl.define do
     game
     user { FactoryGirl.create(:white_player) }
   end
+
+  factory :rook do
+    game
+    user { FactoryGirl.create(:white_player) }
+  end
+
+  factory :bishop do
+    game
+    user { FactoryGirl.create(:white_player) }
+  end
+
 end
