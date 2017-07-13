39e8e66f412bcf5ccb49c2cf4590ad6971362166
Alyson Stiffel <alysonstiffel@gmail.com>
2017-07-13 10:03:59 -0400

    board distortion fixed

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,6 @@
 module ApplicationHelper
 
+# needs to be moved to games_helper.rb, file missing?
   def piece_at_location?(pieces, row, col)
     pieces.each do |piece|
       return [piece[:color], piece[:type]] if piece[:x]==col && piece[:y]==row
