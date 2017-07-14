0b7d3a6b9438b226979285ca4dc348920a9e6f24
jiro-sains <jrosains@gmail.com>
2017-07-14 01:22:36 +0000

    all three obstruction methods working

--- app/models/piece.rb
+++ app/models/piece.rb
@@ -13,37 +13,32 @@ class Piece < ApplicationRecord
   scope :knights, -> { where(piece_type: 'Knight') }
   scope :pawns,   -> { where(piece_type: 'Pawn') }
 
-  def is_horizontal_move_blocked(destination_x, destination_y)
-    rook = Rook.find_by(piece_type: 'Rook')
-    destination_x > rook.current_position_x ? dir_x = 1 : dir_x = -1
-    (1..(destination_x - rook.current_position_x).abs).each do |i|
-      x = rook.current_position_x + i * dir_x
-      piece = Piece.find_by(current_position_x: x, current_position_y: rook.current_position_y)
-      return true if piece
+
+  def is_horizontal_move_blocked(destination_x, destination_y)    
+    destination_x > self.current_position_x ? dir_x = 1 : dir_x = -1
+    (1..(destination_x - self.current_position_x).abs).each do |i|
+      x = self.current_position_x + i * dir_x
+      return true if Piece.find_by(current_position_x: x, current_position_y: self.current_position_y)
     end
     false
   end
 
   def is_vertical_move_blocked(destination_x, destination_y)
-    rook = Rook.find_by(piece_type: 'Rook')
-    destination_y > rook.current_position_y ? dir_y = 1 : dir_y = -1
-    (1..(destination_y - rook.current_position_y).abs).each do |i|
-      y = rook.current_position_y + i * dir_y
-      piece = Piece.find_by(current_position_x: rook.current_position_x, current_position_y: y)
-      return true if piece
+    destination_y > self.current_position_y ? dir_y = 1 : dir_y = -1
+    (1..(destination_y - self.current_position_y).abs).each do |i|
+      y = self.current_position_y + i * dir_y
+      return true if Piece.find_by(current_position_x: self.current_position_x, current_position_y: y)
     end
     false
   end
 
   def is_diagonal_move_blocked(destination_x, destination_y)
-    bishop = Bishop.find_by(piece_type: 'Bishop')
-    destination_x > bishop.current_position_x ? dir_x = 1 : dir_x = -1
-    destination_y > bishop.current_position_y ? dir_y = 1 : dir_y = -1
-    (1..(destination_x - bishop.current_position_x).abs).each do |i|
-      x = bishop.current_position_x + i * dir_x
-      y = bishop.current_position_y + i * dir_y
-      piece = Piece.find_by(current_position_x: x, current_position_y: y)
-      return true if piece
+    destination_x > self.current_position_x ? dir_x = 1 : dir_x = -1
+    destination_y > self.current_position_y ? dir_y = 1 : dir_y = -1
+    (1..(destination_x - self.current_position_x).abs).each do |i|
+      x = self.current_position_x + i * dir_x
+      y = self.current_position_y + i * dir_y
+      return true if Piece.find_by(current_position_x: x, current_position_y: y)
     end
     false
   end
