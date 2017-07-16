7aeb7f21ad9df532f579e32cee48876eedbdf2d3
Brian Wilson <bountonw@gmail.com>
2017-07-16 13:27:00 -0700

    Add make_board method to #create

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -12,40 +12,40 @@ class GamesController < ApplicationController
 
   def new
     @game = Game.new
-    @pieces = [
-      { type: "pawn", color: "white", x: 0, y: 1 },
-      { type: "pawn", color: "white", x: 1, y: 1 },
-      { type: "pawn", color: "white", x: 2, y: 1 },
-      { type: "pawn", color: "white", x: 3, y: 1 },
-      { type: "pawn", color: "white", x: 4, y: 1 },
-      { type: "pawn", color: "white", x: 5, y: 1 },
-      { type: "pawn", color: "white", x: 6, y: 1 },
-      { type: "pawn", color: "white", x: 7, y: 1 },
-      { type: "rook", color: "white", x: 0, y: 0 },
-      { type: "rook", color: "white", x: 7, y: 0 },
-      { type: "knight", color: "white", x: 1, y: 0 },
-      { type: "knight", color: "white", x: 6, y: 0 },
-      { type: "bishop", color: "white", x: 2, y: 0 },
-      { type: "bishop", color: "white", x: 5, y: 0 },
-      { type: "king", color: "white", x: 4, y: 0 },
-      { type: "queen", color: "white", x: 3, y: 0 },
-      { type: "pawn", color: "black", x: 0, y: 6 },
-      { type: "pawn", color: "black", x: 1, y: 6 },
-      { type: "pawn", color: "black", x: 2, y: 6 },
-      { type: "pawn", color: "black", x: 3, y: 6 },
-      { type: "pawn", color: "black", x: 4, y: 6 },
-      { type: "pawn", color: "black", x: 5, y: 6 },
-      { type: "pawn", color: "black", x: 6, y: 6 },
-      { type: "pawn", color: "black", x: 7, y: 6 },
-      { type: "rook", color: "black", x: 7, y: 7 },
-      { type: "rook", color: "black", x: 0, y: 7 },
-      { type: "knight", color: "black", x: 1, y: 7 },
-      { type: "knight", color: "black", x: 6, y: 7 },
-      { type: "bishop", color: "black", x: 2, y: 7 },
-      { type: "bishop", color: "black", x: 5, y: 7 },
-      { type: "king", color: "black", x: 4, y: 7 },
-      { type: "queen", color: "black", x: 3, y: 7 },
-    ]
+    # @pieces = [
+    #   { type: "pawn", color: "white", x: 0, y: 1 },
+    #   { type: "pawn", color: "white", x: 1, y: 1 },
+    #   { type: "pawn", color: "white", x: 2, y: 1 },
+    #   { type: "pawn", color: "white", x: 3, y: 1 },
+    #   { type: "pawn", color: "white", x: 4, y: 1 },
+    #   { type: "pawn", color: "white", x: 5, y: 1 },
+    #   { type: "pawn", color: "white", x: 6, y: 1 },
+    #   { type: "pawn", color: "white", x: 7, y: 1 },
+    #   { type: "rook", color: "white", x: 0, y: 0 },
+    #   { type: "rook", color: "white", x: 7, y: 0 },
+    #   { type: "knight", color: "white", x: 1, y: 0 },
+    #   { type: "knight", color: "white", x: 6, y: 0 },
+    #   { type: "bishop", color: "white", x: 2, y: 0 },
+    #   { type: "bishop", color: "white", x: 5, y: 0 },
+    #   { type: "king", color: "white", x: 4, y: 0 },
+    #   { type: "queen", color: "white", x: 3, y: 0 },
+    #   { type: "pawn", color: "black", x: 0, y: 6 },
+    #   { type: "pawn", color: "black", x: 1, y: 6 },
+    #   { type: "pawn", color: "black", x: 2, y: 6 },
+    #   { type: "pawn", color: "black", x: 3, y: 6 },
+    #   { type: "pawn", color: "black", x: 4, y: 6 },
+    #   { type: "pawn", color: "black", x: 5, y: 6 },
+    #   { type: "pawn", color: "black", x: 6, y: 6 },
+    #   { type: "pawn", color: "black", x: 7, y: 6 },
+    #   { type: "rook", color: "black", x: 7, y: 7 },
+    #   { type: "rook", color: "black", x: 0, y: 7 },
+    #   { type: "knight", color: "black", x: 1, y: 7 },
+    #   { type: "knight", color: "black", x: 6, y: 7 },
+    #   { type: "bishop", color: "black", x: 2, y: 7 },
+    #   { type: "bishop", color: "black", x: 5, y: 7 },
+    #   { type: "king", color: "black", x: 4, y: 7 },
+    #   { type: "queen", color: "black", x: 3, y: 7 },
+    # ]
   end
 
   def create
@@ -57,6 +57,7 @@ class GamesController < ApplicationController
     else
       render :new, status: :unprocessable_entity # 422
     end
+    GameBoard.make_board
   end
 
   def update
