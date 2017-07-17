112e298c7bb35cc3d9f569910b59fe509f34659b
Brian Wilson <bountonw@gmail.com>
2017-07-17 13:12:39 -0700

    Add blank valid_move? method with blank specs

--- app/models/king.rb
+++ app/models/king.rb
@@ -1,3 +1,18 @@
 class King < Piece
+  # def self.valid_move? 
+  #   if 
 
+  #   elsif
+
+  #   else
+  #     return true
+
+  #   end
+    # horizontal +1
+    # if destination x or y > start + 1.absolute || move blocked then false
+    # if diagnal destination.abs > 1 || blocked than false
+    # return true
+    # vertical +1
+    # diagonal +1
+    # is_move_blocked(destination_x, destination_y)
 end
--- spec/models/king_spec.rb
+++ spec/models/king_spec.rb
@@ -4,4 +4,10 @@ RSpec.describe King, type: :model do
   it "King should inherit behavior from Piece" do 
     expect(King.superclass).to eq(Piece)
   end
+
+  describe "#valid_move?" do 
+    it "" do
+
+    end
+  end
 end
