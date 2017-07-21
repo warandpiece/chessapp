22d09ba37db0da9332e29f0cb46a001967e5bc0f
Brian Wilson <bountonw@gmail.com>
2017-07-21 13:41:49 -0700

    valid_move? for pieces with refactoring of king

--- app/models/king.rb
+++ app/models/king.rb
@@ -1,14 +1,7 @@
 class King < Piece
-  def valid_move?(destination_x, destination_y)
-    if destination_x < 0 || destination_x > 7 || destination_y < 0 || destination_y > 7
-      return false
-    elsif is_move_blocked(destination_x, destination_y) == true
-      return false
-    elsif (self.current_position_x - destination_x).abs <= 1 && 
+  def valid_move_king?(destination_x, destination_y)
+    return false if valid_move?(destination_x, destination_y) == false
+    (self.current_position_x - destination_x).abs <= 1 && 
           (self.current_position_y - destination_y).abs <= 1
-      return true
-    else 
-      return false
-    end
   end
 end
