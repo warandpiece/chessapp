7f1defc90862f4839e9d2b2d26b936b3d53514b7
Brian Wilson <bountonw@gmail.com>
2017-07-28 08:08:43 -0700

    Add Game.check method (#55)

--- spec/models/game_spec.rb
+++ spec/models/game_spec.rb
@@ -58,4 +58,56 @@ RSpec.describe Game, type: :model do
       expect(Game.available.map(&:id)[0]).to eq(game.id)  
     end
   end
+
+  # CHECK METHOD
+
+  describe "#check" do                                  
+    it "white in check" do 
+      game = FactoryGirl.create(:game, :no_pieces)
+      white_king = Piece.create(piece_type: 'King', piece_color: 'white', game_id: game.id,
+                          user_id: game.white_player_id, current_position_x: 4, 
+                          current_position_y: 0)
+      black_king = Piece.create(piece_type: 'King', piece_color: 'black', game_id: game.id,
+                          user_id: game.black_player_id, current_position_x: 4, 
+                          current_position_y: 7)
+      black_rook = Piece.create(piece_type: 'Rook', piece_color: 'black', game_id: game.id,
+                          user_id: game.black_player_id, current_position_x: 0, 
+                          current_position_y: 0)
+
+
+      expect(game.check).to eq([:white_player, true])
+    end
+
+    it "black in check" do 
+      game = FactoryGirl.create(:game, :no_pieces)
+      white_king = Piece.create(piece_type: 'King', piece_color: 'white', game_id: game.id,
+                          user_id: game.white_player_id, current_position_x: 4, 
+                          current_position_y: 0)
+      black_king = Piece.create(piece_type: 'King', piece_color: 'black', game_id: game.id,
+                          user_id: game.black_player_id, current_position_x: 4, 
+                          current_position_y: 7)
+      white_rook = Piece.create(piece_type: 'Rook', piece_color: 'white', game_id: game.id,
+                          user_id: game.white_player_id, current_position_x: 0, 
+                          current_position_y: 7)
+
+
+      expect(game.check).to eq([:black_player, true])
+    end
+
+    it "not in check" do 
+      game = FactoryGirl.create(:game, :no_pieces)
+      white_king = Piece.create(piece_type: 'King', piece_color: 'white', game_id: game.id,
+                          user_id: game.white_player_id, current_position_x: 4, 
+                          current_position_y: 0)
+      black_king = Piece.create(piece_type: 'King', piece_color: 'black', game_id: game.id,
+                          user_id: game.black_player_id, current_position_x: 4, 
+                          current_position_y: 7)
+      white_bishop = Piece.create(piece_type: 'Bishop', piece_color: 'white', game_id: game.id,
+                          user_id: game.white_player_id, current_position_x: 0, 
+                          current_position_y: 7)
+
+
+      expect(game.check).to eq(false)
+    end
+  end
 end
